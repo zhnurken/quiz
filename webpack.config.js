fce10f860aeb6838b709a5031602f2f8a0f2ed4f
Nurken Zhappargaly <zhnurken@gmail.com>
2023-04-24 15:55:42 +0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/scripts.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
